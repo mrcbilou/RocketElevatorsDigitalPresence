db2d2bf84bbdf617bea5b4c1e3efb95a39e900e6
Louis-Felix Beland <louisfelix95@hotmail.com>
2020-09-14 16:01:52 -0400

    Fixed formhider.js V 1.0.5

--- assets/js/formhider.js
+++ assets/js/formhider.js
@@ -3,24 +3,29 @@
   function showDiv(select){
     if(select.value==0){
         document.getElementById("residential-form-group").style.display = "none";
+        document.getElementById("lift-cage-num-estimate").value="";
     }
     if(select.value==1){
      document.getElementById("residential-form-group").style.display = "block";
+     document.getElementById("lift-cage-num-estimate").value="";
     } else{
      document.getElementById("residential-form-group").style.display = "none";
     }  
     if(select.value==2){
         document.getElementById("commercial-form-group").style.display = "block";
+        document.getElementById("lift-cage-num-estimate").value="";
     } else{
         document.getElementById("commercial-form-group").style.display = "none";
     }
     if(select.value==3){
             document.getElementById("corporate-form-group").style.display = "block";
+            document.getElementById("lift-cage-num-estimate").value="";
     } else{
         document.getElementById("corporate-form-group").style.display = "none";
      }  
      if(select.value==4){
         document.getElementById("hybrid-form-group").style.display = "block";
+        document.getElementById("lift-cage-num-estimate").value="";
     } else{
         document.getElementById("hybrid-form-group").style.display = "none";
     }  
